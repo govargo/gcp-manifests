5d93010e444e70284480ad75be0ddda7d2020bf6
Kent Iso <kentaiso@google.com>
2023-04-30 02:34:57 +0000

    Add memorystore terraform

---
+++ terraform/memorystore/main.tf
@@ -0,0 +1,49 @@
+module "memorystore" {
+  source  = "terraform-google-modules/memorystore/google"
+  version = "7.1.0"
+
+  name          = "${var.env}-redis"
+  project       = var.gcp_project_id
+  enable_apis   = var.enable_apis
+  region        = var.region
+  redis_version = "REDIS_6_X"
+
+  reserved_ip_range  = "google-managed-services-${var.gcp_project_name}"
+  authorized_network = "projects/${var.gcp_project_id}/global/networks/${var.gcp_project_name}"
+
+  tier         = var.tier
+  auth_enabled = var.auth_enabled
+  connect_mode = var.connect_mode
+  display_name = "${var.env}-redis"
+  labels = {
+    env = "production"
+  }
+  memory_size_gb          = 1
+  read_replicas_mode      = var.read_replicas_mode
+  replica_count           = 1
+  transit_encryption_mode = var.transit_encryption_mode
+
+  maintenance_policy = {
+    ## This is SUNDAY 00:00 in JST due to UTC
+    day = "SATURDAY"
+    start_time = {
+      hours   = 15
+      minutes = 0
+      seconds = 0
+      nanos   = 0
+    }
+  }
+}
+
+resource "google_dns_record_set" "redis_endpoint" {
+  project      = var.gcp_project_id
+  managed_zone = "${var.gcp_project_name}-org"
+
+  name = "memorystore-redis-0.${var.gcp_project_name}.org."
+  type = "A"
+  ttl  = 60
+
+  rrdatas = [module.memorystore.host]
+
+  depends_on = [module.memorystore.host]
+}
---
+++ terraform/memorystore/variables.tf
@@ -0,0 +1,62 @@
+variable "gcp_project_id" {
+  description = "GCP project id"
+  type        = string
+}
+
+variable "gcp_project_name" {
+  description = "GCP project name"
+  type        = string
+}
+
+variable "env" {
+  description = "environment(e.g. dev, stg, prod)"
+  type        = string
+}
+
+variable "region" {
+  description = "Region"
+  type        = string
+  default     = "asia-northeast1"
+}
+
+variable "zone" {
+  description = "Zone"
+  type        = string
+  default     = "asia-northeast1-a"
+}
+
+variable "auth_enabled" {
+  description = "Indicates whether OSS Redis AUTH is enabled for the instance. If set to true AUTH is enabled on the instance"
+  type        = bool
+  default     = false
+}
+
+variable "connect_mode" {
+  description = "The connection mode of the Redis instance. Can be either DIRECT_PEERING or PRIVATE_SERVICE_ACCESS"
+  type        = string
+  default     = "PRIVATE_SERVICE_ACCESS"
+}
+
+variable "enable_apis" {
+  description = "Flag for enabling redis.googleapis.com in your project"
+  type        = bool
+  default     = false
+}
+
+variable "read_replicas_mode" {
+  description = "Read replicas mode"
+  type        = string
+  default     = "READ_REPLICAS_DISABLED"
+}
+
+variable "tier" {
+  description = "The service tier of the instance"
+  type        = string
+  default     = "STANDARD_HA"
+}
+
+variable "transit_encryption_mode" {
+  description = "The TLS mode of the Redis instance"
+  type        = string
+  default     = "DISABLED"
+}
