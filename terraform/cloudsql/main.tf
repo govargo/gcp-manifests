f39ff9ce34c960634736496abf543eb348a73f1f
Kent Iso <kentaiso@google.com>
2023-05-11 10:17:32 +0000

    Update Cloud SQL for production ready

--- terraform/cloudsql/main.tf
+++ terraform/cloudsql/main.tf
@@ -1,12 +1,9 @@
-locals {
-  read_replica_ip_configuration = {
-    ipv4_enabled                                  = var.ipv4_enabled
-    require_ssl                                   = var.require_ssl
-    private_network                               = "projects/${var.gcp_project_id}/global/networks/${var.gcp_project_name}"
-    allocated_ip_range                            = "google-managed-services-${var.gcp_project_name}"
-    authorized_networks                           = []
-    enable_private_path_for_google_cloud_services = true
-  }
+data "google_secret_manager_secret_version" "mysql_root_password" {
+  secret = "mysql_root_password"
+}
+
+data "google_secret_manager_secret_version" "mysql_user_password" {
+  secret = "mysql_user_password"
 }
 
 module "private-service-access" {
@@ -19,7 +16,7 @@ module "private-service-access" {
   prefix_length = 16
 }
 
-module "private-mysql-db" {
+module "cloudsql_mysql" {
   source               = "GoogleCloudPlatform/sql-db/google//modules/mysql"
   version              = "15.0.0"
   name                 = "${var.env}-mysql-instance"
@@ -37,7 +34,7 @@ module "private-mysql-db" {
   maintenance_window_hour         = 12
   maintenance_window_update_track = "stable"
 
-  ##database_flags = [{ name = "long_query_time", value = 1 }]
+  database_flags = [{ name = "cloudsql_iam_authentication", value = "on" }]
 
   disk_autoresize       = true
   disk_autoresize_limit = 100
@@ -65,45 +62,48 @@ module "private-mysql-db" {
   }
 
   backup_configuration = {
-    enabled                        = true
-    binary_log_enabled             = true
-    start_time                     = "20:55"
+    enabled                        = false
+    binary_log_enabled             = false
+    point_in_time_recovery_enabled = false
+    start_time                     = "23:00"
     location                       = null
-    transaction_log_retention_days = null
-    retained_backups               = 365
+    transaction_log_retention_days = 1
+    retained_backups               = 1
     retention_unit                 = "COUNT"
   }
-
-  ## Read replica configurations
-  read_replica_name_suffix = "-read-replica"
-  replica_database_version = var.database_version
-  read_replicas = [
-    {
-      name              = "0"
-      zone              = var.zone
-      availability_type = "ZONAL"
-      tier              = var.tier
-      ip_configuration  = local.read_replica_ip_configuration
-      insights_config = {
-        query_string_length     = 1024
-        record_application_tags = true
-        record_client_address   = true
-      }
-      database_flags        = []
-      disk_autoresize       = true
-      disk_autoresize_limit = 100
-      disk_size             = 10
-      disk_type             = "PD_HDD"
-      user_labels           = { env = "production", role = "read-replica" }
-      encryption_key_name   = null
-    }
-  ]
 
   db_name      = "master_data"
   db_charset   = "utf8mb4"
   db_collation = "utf8mb4_bin"
 
-  user_name = "application"
+  user_name     = "root"
+  user_password = data.google_secret_manager_secret_version.mysql_root_password.secret_data
+  root_password = data.google_secret_manager_secret_version.mysql_root_password.secret_data
 
   depends_on = [module.private-service-access.peering_completed]
+}
+
+resource "google_sql_user" "users" {
+  project = var.gcp_project_id
+
+  name     = "little-quest"
+  host     = "%"
+  instance = module.cloudsql_mysql.instance_name
+  type     = "BUILT_IN"
+  password = data.google_secret_manager_secret_version.mysql_user_password.secret_data
+
+  depends_on = [module.cloudsql_mysql]
+}
+
+resource "google_dns_record_set" "mysql_primary_endpoint" {
+  project      = var.gcp_project_id
+  managed_zone = "${var.gcp_project_name}-org"
+
+  name = "cloudsql-mysql-primary.${var.gcp_project_name}.org."
+  type = "A"
+  ttl  = 60
+
+  rrdatas = [module.cloudsql_mysql.private_ip_address]
+
+  depends_on = [module.cloudsql_mysql.private_ip_address]
 }
