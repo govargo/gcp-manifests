2e3cd37fa8224c6daf9f2130859ce0a34481a387
Kent Iso <kentaiso@google.com>
2023-11-11 12:00:28 +0000

    Add Cloud Armor for prevent security attack

--- terraform/armor/main.tf
+++ terraform/armor/main.tf
@@ -1,55 +1,30 @@
 data "google_project" "project" {
 }
 
-resource "google_compute_security_policy" "owasp_top10_policy" {
-  name    = "${var.env}-owasp10-policy"
+resource "google_compute_security_policy" "little_quest_agent_restrict_policy" {
+  name    = "${var.env}-little-quest-agent-restrict-policy"
   project = data.google_project.project.project_id
 
   type        = "CLOUD_ARMOR"
-  description = "Security Policy for OWASP Top 10 risks"
+  description = "Security Policy for Little Quest with specific agent restrict"
 
-  rule {
-    action      = "deny(403)"
-    description = "OWASP TOP10 Risk (1)"
-    preview     = false
-    priority    = 1100
-
-    match {
-      expr {
-        expression = <<-EOT
-                        evaluatePreconfiguredExpr("sqli-v33-stable")
-                        || evaluatePreconfiguredExpr("xss-v33-stable")
-                        || evaluatePreconfiguredExpr("lfi-v33-stable")
-                        || evaluatePreconfiguredExpr("rfi-v33-stable")
-                        || evaluatePreconfiguredExpr("rce-v33-stable")
-        EOT
-      }
-    }
-  }
-
-  rule {
-    action      = "deny(403)"
-    description = "OWASP TOP10 Risk (2)"
-    preview     = false
-    priority    = 1200
-
-    match {
-      expr {
-        expression = <<-EOT
-                        evaluatePreconfiguredExpr("methodenforcement-v33-stable")
-                        || evaluatePreconfiguredExpr("scannerdetection-v33-stable")
-                        || evaluatePreconfiguredExpr("protocolattack-v33-stable")
-                        || evaluatePreconfiguredExpr("php-v33-stable")
-                        || evaluatePreconfiguredExpr("sessionfixation-v33-stable")
-        EOT
-      }
-    }
-  }
-
-  # Default
   rule {
     action      = "allow"
-    description = "default rule"
+    description = "Unity application can access to the backend"
+    preview     = false
+    priority    = 1000
+
+    match {
+      expr {
+        expression = "request.headers['user-agent'].contains('UnityPlayer')"
+      }
+    }
+  }
+
+  ## Default Rule
+  rule {
+    action      = "deny(403)"
+    description = "Default rule, higher priority overrides it"
     preview     = false
     priority    = 2147483647
 
@@ -62,3 +37,38 @@ resource "google_compute_security_policy" "owasp_top10_policy" {
   }
 }
 
+resource "google_compute_security_policy" "argocd_restrict_policy" {
+  name    = "${var.env}-argocd-agent-restrict-policy"
+  project = data.google_project.project.project_id
+
+  type        = "CLOUD_ARMOR"
+  description = "Security Policy for ArgoCD with specific agent restrict"
+
+  rule {
+    action      = "allow"
+    description = "Chrome can access to the backend"
+    preview     = false
+    priority    = 1000
+
+    match {
+      expr {
+        expression = "request.headers['user-agent'].contains('Chrome')"
+      }
+    }
+  }
+
+  ## Default Rule
+  rule {
+    action      = "deny(403)"
+    description = "Default rule, higher priority overrides it"
+    preview     = false
+    priority    = 2147483647
+
+    match {
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = ["*"]
+      }
+    }
+  }
+}
