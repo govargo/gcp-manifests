df2772a50875ec849ebe8e9ebcade9482cbceb9d
Kent Iso <kentaiso@google.com>
2023-11-25 08:54:59 +0000

    Add update_policy for PROACTIVE update in MIG

--- terraform/datastream/main.tf
+++ terraform/datastream/main.tf
@@ -96,8 +96,8 @@ sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 sudo apt -y install wget
 wget https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v2.7.2/cloud-sql-proxy.linux.amd64 -O /usr/local/bin/cloud-sql-proxy
 chmod +x /usr/local/bin/cloud-sql-proxy
-cloud-sql-proxy --http-address=0.0.0.0 --address 0.0.0.0 --port 3306 --private-ip --telemetry-project=${data.google_project.project.project_id} \
-  --structured-logs --max-sigterm-delay=10s --disable-traces=true --disable-metrics=true --health-check=true \
+cloud-sql-proxy --http-address=0.0.0.0 --address 0.0.0.0 --port 3306 --private-ip \
+  --structured-logs --max-sigterm-delay=10s --health-check=true \
   ${data.google_project.project.project_id}:${var.region}:prod-mysql-instance
 EOF
 
@@ -131,6 +131,12 @@ resource "google_compute_instance_group_manager" "datastream_cloudsql_proxy_mig"
 
   wait_for_instances        = true
   wait_for_instances_status = "STABLE"
+
+  update_policy {
+    type                    = "PROACTIVE"
+    replacement_method      = "SUBSTITUTE"
+    minimal_action          = "REPLACE"
+  }
 }
 
 resource "time_sleep" "wait_60_seconds" {
