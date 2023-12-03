9813646b09d541ac95fc14c994c42fc0946ab7a9
Kent Iso <kentaiso@google.com>
2023-12-03 07:54:50 +0000

    Add trigger for little-quest-realtime chart

--- terraform/build/build.tf
+++ terraform/build/build.tf
@@ -61,3 +61,24 @@ resource "google_cloudbuild_trigger" "little_quest_realtime_build_trigger" {
 
   include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
 }
+
+resource "google_cloudbuild_trigger" "little_quest_realtime_helm_chart_build_trigger" {
+  project  = data.google_project.project.project_id
+  name     = "little-quest-realtime-helm-chart-build-trigger"
+  location = var.region
+  filename = "k8s/helm/little-quest-realtime/cloudbuild.yaml"
+
+  github {
+    owner = "govargo"
+    name  = "gcp-manifests"
+    push {
+      tag = "^little-quest-realtime-[0-9].[0-9].[0-9]$"
+    }
+  }
+
+  substitutions = {
+    "_BUCKET_NAME" = data.google_project.project.project_id
+  }
+
+  include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+}
