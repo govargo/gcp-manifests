0fb4725cf34c6d6db3d3769476a12ef0b1cd6406
Kent Iso <kentaiso@google.com>
2023-12-11 05:56:49 +0000

    Add service account for little-quest-director

--- terraform/gke/corp/corp.tf
+++ terraform/gke/corp/corp.tf
@@ -262,6 +262,34 @@ module "little_quest_mmf_workloadIdentity_binding" {
   depends_on = [module.little_quest_mmf_sa]
 }
 
+module "little_quest_director_sa" {
+  source     = "terraform-google-modules/service-accounts/google"
+  version    = "4.2.2"
+  project_id = data.google_project.project.project_id
+
+  names        = ["little-quest-director"]
+  display_name = "Little Quest Director ServiceAccount"
+  project_roles = [
+    "${data.google_project.project.project_id}=>roles/cloudprofiler.agent",
+    "${data.google_project.project.project_id}=>roles/cloudtrace.agent",
+  ]
+}
+
+module "little_quest_director_workloadIdentity_binding" {
+  source  = "terraform-google-modules/iam/google//modules/service_accounts_iam"
+  version = "7.7.1"
+  project = data.google_project.project.project_id
+
+  service_accounts = [module.little_quest_director_sa.email]
+  mode             = "additive"
+  bindings = {
+    "roles/iam.workloadIdentityUser" = [
+      "serviceAccount:${data.google_project.project.project_id}.svc.id.goog[corp-0/little-quest-director]"
+    ]
+  }
+  depends_on = [module.little_quest_director_sa]
+}
+
 module "agones_allocator_sa" {
   source     = "terraform-google-modules/service-accounts/google"
   version    = "4.2.2"
