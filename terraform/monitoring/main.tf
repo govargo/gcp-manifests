ddbe971d6f22fb5abac42adbb0d5c02094f5635f
Kent Iso <kentaiso@google.com>
2024-02-14 10:12:04 +0000

    Add GKE Cluster monitoring

--- terraform/monitoring/main.tf
+++ terraform/monitoring/main.tf
@@ -7,13 +7,19 @@ resource "google_monitoring_dashboard" "https_loadbalancing_monitoring_dashboard
   dashboard_json = file("files/application_loadbalancing.json")
 }
 
-# Cloud Spanner Instance
+## GKE Cluster
+resource "google_monitoring_dashboard" "gke_cluster_monitoring_dashboard" {
+  project   = data.google_project.project.project_id
+  dashboard_json = file("files/gke_cluster.json")
+}
+
+## Cloud Spanner Instance
 resource "google_monitoring_dashboard" "spanner_instance_monitoring_dashboard" {
   project   = data.google_project.project.project_id
   dashboard_json = file("files/cloud_spanner.json")
 }
 
-# Cloud SQL for MySQL
+## Cloud SQL for MySQL
 resource "google_monitoring_dashboard" "cloudsql_mysql_monitoring_dashboard" {
   project   = data.google_project.project.project_id
   dashboard_json = file("files/cloudsql_mysql.json")
