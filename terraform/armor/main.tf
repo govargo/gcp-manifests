79d965bf30825517d6206832baf3302f74d9061c
Kent Iso <kentaiso@google.com>
2024-04-06 07:28:21 +0000

    Remove Cloud Armor for argocd

--- terraform/armor/main.tf
+++ terraform/armor/main.tf
@@ -36,39 +36,3 @@ resource "google_compute_security_policy" "little_quest_server_agent_restrict_po
     }
   }
 }
-
-resource "google_compute_security_policy" "argocd_restrict_policy" {
-  name    = "${var.env}-argocd-agent-restrict-policy"
-  project = data.google_project.project.project_id
-
-  type        = "CLOUD_ARMOR"
-  description = "Security Policy for ArgoCD with specific agent restrict"
-
-  rule {
-    action      = "allow"
-    description = "Chrome can access to the backend"
-    preview     = false
-    priority    = 1000
-
-    match {
-      expr {
-        expression = "request.headers['user-agent'].contains('Chrome') || request.headers['user-agent'].contains('argocd')"
-      }
-    }
-  }
-
-  ## Default Rule
-  rule {
-    action      = "deny(403)"
-    description = "Default rule, higher priority overrides it"
-    preview     = false
-    priority    = 2147483647
-
-    match {
-      versioned_expr = "SRC_IPS_V1"
-      config {
-        src_ip_ranges = ["*"]
-      }
-    }
-  }
-}
