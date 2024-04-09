6fe014d4e14b30d4e5073907dd022ca8ab3f7732
Kent Iso <kentaiso@google.com>
2024-04-09 12:35:57 +0000

    Add cleanup policy to artifact resitry

--- terraform/build/artifact.tf
+++ terraform/build/artifact.tf
@@ -9,4 +9,35 @@ resource "google_artifact_registry_repository" "docker_repository" {
   repository_id = "little-quest"
   description   = "Docker repository"
   format        = "DOCKER"
+
+  cleanup_policies {
+    id     = "delete-prerelease"
+    action = "DELETE"
+    condition {
+      tag_state             = "TAGGED"
+      tag_prefixes          = []
+      version_name_prefixes = []
+      older_than            = "2678400s"
+    }
+  }
+
+  cleanup_policies {
+    id     = "keep-tagged-release"
+    action = "KEEP"
+    condition {
+      tag_state             = "TAGGED"
+      tag_prefixes          = []
+      version_name_prefixes = []
+      newer_than            = "2592000s"
+    }
+  }
+
+  cleanup_policies {
+    id     = "keep-minimum-versions"
+    action = "KEEP"
+    most_recent_versions {
+      package_name_prefixes = []
+      keep_count            = 5
+    }
+  }
 }
