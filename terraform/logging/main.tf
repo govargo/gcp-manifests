aff74c7fc63a89c8c308c1ac8bfee4652e829da0
Kent Iso <kentaiso@google.com>
2024-06-17 10:12:54 +0000

    Add Cloud Logging exclude for cost reduction

--- terraform/logging/main.tf
+++ terraform/logging/main.tf
@@ -18,6 +18,99 @@ resource "google_logging_project_bucket_config" "_default_bucket_log_analytics_e
   retention_days   = 30
 }
 
+## Exclude Log for Cost Reduction
+resource "google_logging_project_exclusion" "load_balancing_exclude_2xx_code" {
+  name = "load-balancing-exclude-2xx-code"
+
+  description = "Exclude Load Balancing logs for 2xx-3xx status code"
+
+  filter = <<EOF
+resource.type="http_load_balancer" AND severity>=INFO
+httpRequest.status>=200 AND httpRequest.status<=400
+EOF
+}
+
+resource "google_logging_project_exclusion" "k8s_container_exclude_kube_system" {
+  name = "k8s-container-exclude-kube-system"
+
+  description = "Exclude kube-system info log for all clusters"
+
+  filter = <<EOF
+resource.type="k8s_container"
+resource.labels.namespace_name="kube-system"
+severity=INFO
+EOF
+}
+
+resource "google_logging_project_exclusion" "k8s_container_exclude_managed_prometheus" {
+  name = "k8s-container-exclude-managed-prometheus"
+
+  description = "Exclude managed prometheus info log for all clusters"
+
+  filter = <<EOF
+resource.type="k8s_container"
+resource.labels.namespace_name="gmp-system" OR resource.labels.namespace_name="gmp-public"
+severity=INFO
+EOF
+}
+
+resource "google_logging_project_exclusion" "k8s_container_exclude_argocd" {
+  name = "k8s-container-exclude-argocd"
+
+  description = "Exclude argocd all log for all clusters"
+
+  filter = <<EOF
+resource.type="k8s_container"
+resource.labels.namespace_name="argocd"
+EOF
+}
+
+resource "google_logging_project_exclusion" "k8s_container_exclude_falco" {
+  name = "k8s-container-exclude-falco"
+
+  description = "Exclude falco all log for all clusters"
+
+  filter = <<EOF
+resource.type="k8s_container"
+resource.labels.namespace_name="falco"
+EOF
+}
+
+resource "google_logging_project_exclusion" "k8s_container_exclude_gke_mcs" {
+  name = "k8s-container-exclude-gke-mcs"
+
+  description = "Exclude GKE multi cluster service all log for all clusters"
+
+  filter = <<EOF
+resource.type="k8s_container"
+resource.labels.namespace_name="gke-mcs"
+EOF
+}
+
+resource "google_logging_project_exclusion" "k8s_container_exclude_little_quest_server_healthcheck" {
+  name = "k8s-container-exclude-little-quest-server-healthcheck"
+
+  description = "Exclude little quest server health check log"
+
+  filter = <<EOF
+resource.type="k8s_container"
+resource.labels.namespace_name="little-quest-server"
+textPayload=~".*GET /healthz HTTP/1.1.*" OR textPayload=~".*GET /readiness HTTP/1.1.*" OR textPayload=~".*GET /metrics HTTP/1.1.*"
+EOF
+}
+
+resource "google_logging_project_exclusion" "k8s_container_exclude_little_quest_realtime_healthcheck" {
+  name = "k8s-container-exclude-little-quest-realtime-healthcheck"
+
+  description = "Exclude little quest realtime health check log"
+
+  filter = <<EOF
+resource.type="k8s_container"
+resource.labels.namespace_name="corp-0"
+textPayload=~".*GET /healthz HTTP/1.1.*" OR textPayload=~".*GET /readiness HTTP/1.1.*" OR textPayload=~".*GET /metrics HTTP/1.1.*"
+EOF
+}
+
 ## Cloud Logging Sink for Little Quest
 resource "google_logging_project_sink" "kpi_action_log" {
   project = data.google_project.project.project_id
