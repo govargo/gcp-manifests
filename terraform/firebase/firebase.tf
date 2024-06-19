dce0ffeed71ff05d47da8cc65d9f78dc343ec189
Kent Iso <kentaiso@google.com>
2024-06-19 09:55:08 +0000

    Update firebase resource and add security rule for terraform

--- terraform/firebase/firebase.tf
+++ terraform/firebase/firebase.tf
@@ -6,12 +6,3 @@ resource "google_firebase_project" "little_quest" {
   provider = google-beta
   project  = data.google_project.project.project_id
 }
-
-resource "google_firebase_project_location" "fireabase_location" {
-  provider = google-beta
-  project  = google_firebase_project.little_quest.project
-
-  location_id = var.region
-
-  depends_on = [google_firebase_project.little_quest]
-}
--- terraform/firebase/firestore.tf
+++ terraform/firebase/firestore.tf
@@ -8,3 +8,53 @@ resource "google_firestore_database" "default" {
   concurrency_mode            = "OPTIMISTIC"
   app_engine_integration_mode = "DISABLED"
 }
+
+resource "google_firestore_field" "update_at_ttl" {
+  project    = data.google_project.project.project_id
+  database   = google_firestore_database.default.name
+  collection = "messages"
+  field      = "updateAt"
+
+  # enables a TTL policy for the document based on the value of entries with this field
+  ttl_config {}
+}
+
+resource "google_firebaserules_ruleset" "firestore" {
+  project = data.google_project.project.project_id
+
+  source {
+    files {
+      name    = "firestore.rules"
+      content = <<EOF
+rules_version = '2';
+service cloud.firestore {
+  match /databases/{database}/documents {
+    function isValidCreated() {
+      return
+        request.resource.data.createdAt is timestamp &&
+        request.resource.data.createdBy is string &&
+        request.resource.data.createdBy.size() > 0 &&
+        request.resource.data.updatedAt is timestamp &&
+        request.resource.data.updatedBy.size() > 0
+    }
+
+    function isValidUpdated() {
+      return
+        request.resource.data.createdAt is timestamp &&
+        request.resource.data.createdBy is string &&
+        request.resource.data.createdBy.size() > 0 &&
+        request.resource.data.updatedAt is timestamp &&
+        request.resource.data.updatedBy.size() > 0
+    }
+
+    match /messages/{message=**} {
+      allow read: if true
+      allow create: if isValidCreated()
+      allow update: if isValidUpdated()
+    }
+  }
+}
+EOF
+    }
+  }
+}
