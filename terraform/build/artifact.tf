88a2283e2c9e0977bf587d18adddce058287a88e
Kent Iso <kentaiso@google.com>
2024-06-20 10:23:17 +0000

    Add artifact registry for open-match

--- terraform/build/artifact.tf
+++ terraform/build/artifact.tf
@@ -2,12 +2,12 @@ data "google_project" "project" {
 }
 
 ## Artifact Registry
-resource "google_artifact_registry_repository" "docker_repository" {
+resource "google_artifact_registry_repository" "docker_repository_little_quest" {
   project = data.google_project.project.project_id
 
   location      = var.region
   repository_id = "little-quest"
-  description   = "Docker repository"
+  description   = "Docker repository for Little Quest"
   format        = "DOCKER"
 
   cleanup_policies {
@@ -38,6 +38,46 @@ resource "google_artifact_registry_repository" "docker_repository" {
     most_recent_versions {
       package_name_prefixes = []
       keep_count            = 5
+    }
+  }
+}
+
+resource "google_artifact_registry_repository" "docker_repository_open_match" {
+  project = data.google_project.project.project_id
+
+  location      = var.region
+  repository_id = "open-match"
+  description   = "Docker repository for Open Match"
+  format        = "DOCKER"
+
+  cleanup_policies {
+    id     = "delete-prerelease"
+    action = "DELETE"
+    condition {
+      tag_state             = "TAGGED"
+      tag_prefixes          = []
+      version_name_prefixes = []
+      older_than            = "2678400s"
+    }
+  }
+
+  cleanup_policies {
+    id     = "keep-tagged-release"
+    action = "KEEP"
+    condition {
+      tag_state             = "TAGGED"
+      tag_prefixes          = []
+      version_name_prefixes = []
+      newer_than            = "2592000s"
+    }
+  }
+
+  cleanup_policies {
+    id     = "keep-minimum-versions"
+    action = "KEEP"
+    most_recent_versions {
+      package_name_prefixes = []
+      keep_count            = 2
     }
   }
 }
