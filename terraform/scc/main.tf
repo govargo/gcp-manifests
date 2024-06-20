bedfa73d1a83359a53dd5dc534760c45c5e90338
Kent Iso <kentaiso@google.com>
2024-06-20 14:05:15 +0000

    Add mute config to SCC

--- terraform/scc/main.tf
+++ terraform/scc/main.tf
@@ -7,3 +7,73 @@ resource "google_scc_mute_config" "open_firewall_agones_gameserver" {
   filter         = "resource.name=\"//compute.googleapis.com/projects/${data.google_project.project.project_id}/global/firewalls/3788064070487107887\""
   description    = "Agones gameserver require open ports range, so mute this Open firewall alert"
 }
+
+resource "google_scc_mute_config" "open_firewall_deny_all_ingress" {
+  mute_config_id = "mute-firewall-deny-all-ingress"
+  parent         = "projects/${data.google_project.project.project_id}"
+  filter         = "resource.name=\"//compute.googleapis.com/projects/${data.google_project.project.project_id}/global/firewalls/5930557593548618531\""
+  description    = "Deny all ingress policy is a deny policy not allow, so mute this Open firewall alert"
+}
+
+resource "google_scc_mute_config" "gke_host_namespaces_app_0_node_exporter" {
+  mute_config_id = "mute-gke-host-namespace-app-0-node-exporter"
+  parent         = "projects/${data.google_project.project.project_id}"
+  filter         = "contains(kubernetes.objects, name=\"app-0-prometheus-node-exporter\") AND category=\"GKE_HOST_NAMESPACES\""
+  description    = "Node exporter require host namespace, so mute this GKE host namespace alert"
+}
+
+resource "google_scc_mute_config" "gke_host_namespaces_corp_0_node_exporter" {
+  mute_config_id = "mute-gke-host-namespace-corp-0-node-exporter"
+  parent         = "projects/${data.google_project.project.project_id}"
+  filter         = "contains(kubernetes.objects, name=\"corp-0-prometheus-node-exporter\") AND category=\"GKE_HOST_NAMESPACES\""
+  description    = "Node exporter require host namespace, so mute this GKE host namespace alert"
+}
+
+resource "google_scc_mute_config" "gke_host_namespaces_misc_0_node_exporter" {
+  mute_config_id = "mute-gke-host-namespace-misc-0-node-exporter"
+  parent         = "projects/${data.google_project.project.project_id}"
+  filter         = "contains(kubernetes.objects, name=\"misc-0-prometheus-node-exporter\") AND category=\"GKE_HOST_NAMESPACES\""
+  description    = "Node exporter require host namespace, so mute this GKE host namespace alert"
+}
+
+resource "google_scc_mute_config" "gke_host_ports_app_0_node_exporter" {
+  mute_config_id = "mute-gke-host-port-app-0-node-exporter"
+  parent         = "projects/${data.google_project.project.project_id}"
+  filter         = "contains(kubernetes.objects, name=\"app-0-prometheus-node-exporter\") AND category=\"GKE_HOST_PORTS\""
+  description    = "Node exporter require host port, so mute this GKE host port alert"
+}
+
+resource "google_scc_mute_config" "gke_host_ports_corp_0_node_exporter" {
+  mute_config_id = "mute-gke-host-port-corp-0-node-exporter"
+  parent         = "projects/${data.google_project.project.project_id}"
+  filter         = "contains(kubernetes.objects, name=\"corp-0-prometheus-node-exporter\") AND category=\"GKE_HOST_PORTS\""
+  description    = "Node exporter require host port, so mute this GKE host port alert"
+}
+
+resource "google_scc_mute_config" "gke_host_ports_misc_0_node_exporter" {
+  mute_config_id = "mute-gke-host-port-misc-0-node-exporter"
+  parent         = "projects/${data.google_project.project.project_id}"
+  filter         = "contains(kubernetes.objects, name=\"misc-0-prometheus-node-exporter\") AND category=\"GKE_HOST_PORTS\""
+  description    = "Node exporter require host port, so mute this GKE host port alert"
+}
+
+resource "google_scc_mute_config" "gke_privileged_containers_app_0" {
+  mute_config_id = "mute-gke-privileged-containers-app-0"
+  parent         = "projects/${data.google_project.project.project_id}"
+  filter         = "(contains(kubernetes.objects, name=\"app-0-falco\") AND contains(kubernetes.objects, name=\"secrets-store-csi-driver\")) AND category=\"GKE_PRIVILEGED_CONTAINERS\""
+  description    = "falco and secret csi driver require privileged, so mute this GKE privileged container alert"
+}
+
+resource "google_scc_mute_config" "gke_privileged_containers_corp_0" {
+  mute_config_id = "mute-gke-privileged-containers-corp-0"
+  parent         = "projects/${data.google_project.project.project_id}"
+  filter         = "(contains(kubernetes.objects, name=\"corp-0-falco\") AND contains(kubernetes.objects, name=\"secrets-store-csi-driver\")) AND category=\"GKE_PRIVILEGED_CONTAINERS\""
+  description    = "falco and secret csi driver require privileged, so mute this GKE privileged container alert"
+}
+
+resource "google_scc_mute_config" "gke_privileged_containers_misc_0" {
+  mute_config_id = "mute-gke-privileged-containers-misc-0"
+  parent         = "projects/${data.google_project.project.project_id}"
+  filter         = "(contains(kubernetes.objects, name=\"misc-0-falco\") AND contains(kubernetes.objects, name=\"secrets-store-csi-driver\")) AND category=\"GKE_PRIVILEGED_CONTAINERS\""
+  description    = "falco and secret csi driver require privileged, so mute this GKE privileged container alert"
+}
