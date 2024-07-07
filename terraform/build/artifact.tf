c83c403ccc0d7cde1e9aa6494616fd9ff7b06854
Kent Iso <kentaiso@google.com>
2024-07-07 13:15:32 +0000

    Add multi-cluster-gateway chart and build trigger

--- terraform/build/artifact.tf
+++ terraform/build/artifact.tf
@@ -42,6 +42,46 @@ resource "google_artifact_registry_repository" "docker_repository_little_quest" 
   }
 }
 
+resource "google_artifact_registry_repository" "docker_repository_multi_cluster_gateway" {
+  project = data.google_project.project.project_id
+
+  location      = var.region
+  repository_id = "multi-cluster-gateway"
+  description   = "Docker repository for Multi Cluster Gateway chart"
+  format        = "DOCKER"
+
+  cleanup_policies {
+    id     = "delete-prerelease"
+    action = "DELETE"
+    condition {
+      tag_state             = "TAGGED"
+      tag_prefixes          = []
+      version_name_prefixes = []
+      older_than            = "2678400s"
+    }
+  }
+
+  cleanup_policies {
+    id     = "keep-tagged-release"
+    action = "KEEP"
+    condition {
+      tag_state             = "TAGGED"
+      tag_prefixes          = []
+      version_name_prefixes = []
+      newer_than            = "2592000s"
+    }
+  }
+
+  cleanup_policies {
+    id     = "keep-minimum-versions"
+    action = "KEEP"
+    most_recent_versions {
+      package_name_prefixes = []
+      keep_count            = 5
+    }
+  }
+}
+
 resource "google_artifact_registry_repository" "docker_repository_open_match" {
   project = data.google_project.project.project_id
 
--- terraform/build/build.tf
+++ terraform/build/build.tf
@@ -41,6 +41,27 @@ resource "google_cloudbuild_trigger" "little_quest_server_helm_chart_build_trigg
   include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
 }
 
+resource "google_cloudbuild_trigger" "multi_cluster_gateway_helm_chart_build_trigger" {
+  project  = data.google_project.project.project_id
+  name     = "multi-cluster-gateway-helm-chart-build-trigger"
+  location = var.region
+  filename = "k8s/helm/multi-cluster-gateway/cloudbuild.yaml"
+
+  github {
+    owner = "govargo"
+    name  = "gcp-manifests"
+    push {
+      tag = "^multi-cluster-gateway-[0-9].[0-9].[0-9]+$"
+    }
+  }
+
+  substitutions = {
+    "_BUCKET_NAME" = data.google_project.project.project_id
+  }
+
+  include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+}
+
 resource "google_cloudbuild_trigger" "little_quest_realtime_build_trigger" {
   project  = data.google_project.project.project_id
   name     = "little-quest-realtime-build-trigger"
