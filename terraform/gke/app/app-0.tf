1cdf2c33217830576ea95a2fba8e450e2baf77b0
Kent Iso <kentaiso@google.com>
2024-07-11 11:27:05 +0000

    Change machine type to T2D for cost saving

--- terraform/gke/app/app-0.tf
+++ terraform/gke/app/app-0.tf
@@ -73,7 +73,7 @@ module "app-0" {
   node_pools = [
     {
       name               = "kube-system-pool"
-      machine_type       = "e2-custom-2-2560"
+      machine_type       = "t2d-standard-2"
       node_locations     = var.node_locations
       min_count          = null
       max_count          = null
@@ -99,7 +99,7 @@ module "app-0" {
     },
     {
       name               = "app-pool"
-      machine_type       = "e2-custom-2-3072"
+      machine_type       = "t2d-standard-1"
       node_locations     = var.node_locations
       min_count          = null
       max_count          = null
