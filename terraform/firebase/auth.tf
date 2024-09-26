a8b85e65a7f95da5b9eaf2f5cfc31f5ecc7d1867
Kent Iso <kentaiso@google.com>
2024-09-26 08:00:42 +0000

    Add Quota param for Firebase auth

--- terraform/firebase/auth.tf
+++ terraform/firebase/auth.tf
@@ -10,6 +10,14 @@ resource "google_identity_platform_config" "default" {
     }
   }
 
+  quota {
+    sign_up_quota_config {
+      quota          = 1000
+      quota_duration = "604800s"
+      start_time     = "2024-09-05T08:00:00Z"
+    }
+  }
+
   monitoring {
     request_logging {
       enabled = false
