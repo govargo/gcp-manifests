db77bf84a1ebea1f111bda71c69459e7c59513d4
Kent Iso <kentaiso@google.com>
2024-12-03 09:27:43 +0000

    Add billing export dataset to global

--- terraform/global/main.tf
+++ terraform/global/main.tf
@@ -333,3 +333,32 @@ resource "google_secret_manager_secret" "redis_password" {
   depends_on = [google_project_service.service, time_sleep.wait_150_seconds]
 }
 
+## BigQuery dataset
+resource "google_bigquery_dataset" "billing_export" {
+  project = data.google_project.project.project_id
+
+  dataset_id            = "all_billing_data"
+  friendly_name         = "cloud_billing_billing_export"
+  description           = "Cloud Billing data export to BigQuery"
+  location              = var.region
+  storage_billing_model = "PHYSICAL"
+
+  labels = {
+    role = "billing"
+  }
+}
+
+resource "google_bigquery_dataset" "billing_board" {
+  project = data.google_project.project.project_id
+
+  dataset_id            = "billing_board"
+  friendly_name         = "Cloud Billing Dashboard"
+  description           = "BigQuery dataset where the BigQuery views for the billing dashboard"
+  location              = var.region
+  storage_billing_model = "PHYSICAL"
+
+  labels = {
+    role = "billing"
+  }
+}
+
