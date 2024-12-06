f97fddaf37baba55b4fa283e5bc4ede122f67514
Kent Iso <kentaiso@google.com>
2024-12-06 12:46:40 +0000

    Must create OAuth client credential via Cloud Console

--- terraform/gke/misc/security.tf
+++ terraform/gke/misc/security.tf
@@ -1,21 +1,15 @@
 ## IAP Setting
-#data "google_compute_backend_service" "argocd_backend_service" {
-#  name = "k8s1-f8f020c8-argocd-argocd-server-80-2a46cd0c"
-#}
+data "google_compute_backend_service" "argocd_backend_service" {
+  name = "k8s1-9c978f4e-argocd-argocd-server-80-a640d488"
+}
 
-#resource "google_iap_web_backend_service_iam_binding" "argocd_iap_iam_binding" {
-#  project             = data.google_project.project.project_id
-#  web_backend_service = data.google_compute_backend_service.argocd_backend_service.name
-#  role                = "roles/iap.httpsResourceAccessor"
-#  members = [
-#    "user:admin@kentaiso.altostrat.com",
-#  ]
-#}
-
-## OAuth Client
-resource "google_iap_client" "argocd_iap_oauth_client" {
-  display_name = "ArgoCD"
-  brand        = "projects/${data.google_project.project.number}/brands/${data.google_project.project.number}"
+resource "google_iap_web_backend_service_iam_binding" "argocd_iap_iam_binding" {
+  project             = data.google_project.project.project_id
+  web_backend_service = data.google_compute_backend_service.argocd_backend_service.name
+  role                = "roles/iap.httpsResourceAccessor"
+  members = [
+    "user:admin@kentaiso.altostrat.com",
+  ]
 }
 
 ## Secret
@@ -34,16 +28,6 @@ resource "google_secret_manager_secret" "argocd_client_id" {
   lifecycle {
     prevent_destroy = true
   }
-
-  depends_on = [google_iap_client.argocd_iap_oauth_client]
-}
-
-resource "google_secret_manager_secret_version" "argocd_client_id" {
-  secret = google_secret_manager_secret.argocd_client_id.id
-
-  secret_data = google_iap_client.argocd_iap_oauth_client.client_id
-
-  depends_on = [google_secret_manager_secret.argocd_client_id]
 }
 
 resource "google_secret_manager_secret" "argocd_client_secret" {
@@ -61,16 +45,6 @@ resource "google_secret_manager_secret" "argocd_client_secret" {
   lifecycle {
     prevent_destroy = true
   }
-
-  depends_on = [google_iap_client.argocd_iap_oauth_client]
-}
-
-resource "google_secret_manager_secret_version" "argocd_client_secret" {
-  secret = google_secret_manager_secret.argocd_client_secret.id
-
-  secret_data = google_iap_client.argocd_iap_oauth_client.secret
-
-  depends_on = [google_iap_client.argocd_iap_oauth_client, google_secret_manager_secret.argocd_client_secret]
 }
 
 resource "google_secret_manager_secret" "argocd_notification_webhook_url" {
@@ -89,3 +63,4 @@ resource "google_secret_manager_secret" "argocd_notification_webhook_url" {
     prevent_destroy = true
   }
 }
+
