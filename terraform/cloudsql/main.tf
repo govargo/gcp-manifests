26f6f0605416680dabb1e7567e411be7c35f13cb
Kent Iso <kentaiso@google.com>
2025-04-10 14:39:22 +0000

    Change to Enterprise Plus

--- terraform/cloudsql/main.tf
+++ terraform/cloudsql/main.tf
@@ -34,11 +34,11 @@ module "cloudsql_mysql" {
   deletion_protection = true
 
   database_version                = "MYSQL_8_0_31"
-  edition                         = "ENTERPRISE"
+  edition                         = "ENTERPRISE_PLUS"
   region                          = var.region
   zone                            = var.zone
   secondary_zone                  = "asia-northeast1-b"
-  tier                            = var.tier
+  tier                            = "db-perf-optimized-N-2"
   availability_type               = var.availability_type
   maintenance_window_day          = 7
   maintenance_window_hour         = 12
@@ -48,13 +48,14 @@ module "cloudsql_mysql" {
   database_flags = [
     { name = "log_output", value = "FILE" },
     { name = "slow_query_log", value = "on" },
-    { name = "long_query_time", value = "3" }
+    { name = "long_query_time", value = "3" },
+    { name = "cloudsql_index_advisor", value = "on" }
   ]
 
   disk_autoresize       = true
   disk_autoresize_limit = 100
   disk_size             = 10
-  disk_type             = "PD_HDD"
+  disk_type             = "PD_SSD"
 
   user_labels = {
     env  = "production",
@@ -85,47 +86,9 @@ module "cloudsql_mysql" {
     start_time                     = "23:00"
     location                       = null
     transaction_log_retention_days = 1
-    retained_backups               = 1
+    retained_backups               = 2
     retention_unit                 = "COUNT"
   }
-
-  ## Read replica configurations
-  replica_database_version = "MYSQL_8_0_31"
-  read_replicas = [
-    {
-      name              = "-0"
-      name_override     = "${var.env}-mysql-instance-read-replica-0"
-      edition           = "ENTERPRISE"
-      zone              = var.zone
-      availability_type = "ZONAL"
-      tier              = var.tier
-      ip_configuration = {
-        ipv4_enabled                                  = var.ipv4_enabled
-        enable_private_path_for_google_cloud_services = true
-        ssl_mode                                      = "ENCRYPTED_ONLY"
-        private_network                               = "projects/${data.google_project.project.project_id}/global/networks/${var.gcp_project_name}"
-        allocated_ip_range                            = "google-managed-services-${var.gcp_project_name}"
-        authorized_networks                           = []
-      }
-      backup_configuration = {
-        enabled                        = true
-        binary_log_enabled             = true
-        point_in_time_recovery_enabled = false
-        start_time                     = "23:00"
-        location                       = null
-        transaction_log_retention_days = 1
-        retained_backups               = 1
-        retention_unit                 = "COUNT"
-      }
-      database_flags        = []
-      disk_autoresize       = true
-      disk_autoresize_limit = 100
-      disk_size             = 10
-      disk_type             = "PD_HDD"
-      user_labels           = { env = "production", role = "read-replica" }
-      encryption_key_name   = null
-    }
-  ]
 
   db_name      = "master_data"
   db_charset   = "utf8mb4"
