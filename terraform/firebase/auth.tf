a15be386b570b62a26b31a57824bea2ad85649b0
Kent Iso <kentaiso@google.com>
2025-04-10 03:41:02 +0000

    Update auth.tf

--- terraform/firebase/auth.tf
+++ terraform/firebase/auth.tf
@@ -8,13 +8,24 @@ resource "google_identity_platform_config" "default" {
     anonymous {
       enabled = true
     }
+
+    email {
+      enabled           = false
+      password_required = false
+    }
+
+    phone_number {
+      enabled            = false
+      test_phone_numbers = {}
+    }
+
   }
 
   quota {
     sign_up_quota_config {
       quota          = 1000
       quota_duration = "604800s"
-      start_time     = "2024-12-03T08:00:00Z"
+      start_time     = "2025-02-07T08:06:00Z"
     }
   }
 
